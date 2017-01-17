8969a5e088cfefbf2a8536fbfb82a8e46207f87d
Wesley Klop <wesley19097@gmail.com>
2017-01-17 22:05:41 +0100

    Move to webpack 2

--- app/main.jsx
+++ app/main.jsx
@@ -5,7 +5,7 @@ import 'react-mdl/extra/material.css'
 import { AppContainer as HMRContainer } from 'react-hot-loader'
 import { render } from 'react-dom'
 import DelugeWUIM from './DelugeWUIM'
-import Deluge from './lib/Deluge/Deluge'
+import Deluge from './lib/Deluge'
 import { delugeLocation } from '../settings.json'
 import createAppStore from './store'
 import './main.css'
@@ -13,19 +13,19 @@ import './main.css'
 const appRoot = document.querySelector('#app')
 const deluge = new Deluge({ delugeLocation })
 const store = createAppStore(deluge)
+HMRContainer.displayName = 'HMRContainer'
 
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  HMRContainer.displayName = 'HMRContainer'
-  /* eslint-disable */
-  render(<HMRContainer><DelugeWUIM deluge={deluge} store={store} /></HMRContainer>, appRoot)
+const renderApp = App => render(
+  <HMRContainer>
+    <App deluge={deluge} store={store} />
+  </HMRContainer>,
+  appRoot,
+)
 
-  // HMR
-  if (module.hot) {
-    module.hot.accept('./DelugeWUIM', () => {
-      const DelugeWUIM = require('./DelugeWUIM').default
-      render(<HMRContainer><DelugeWUIM deluge={deluge} store={store} /></HMRContainer>)
-    })
-  }
+renderApp(DelugeWUIM)
+
+if (module.hot) {
+  module.hot.accept('./DelugeWUIM', () => renderApp(DelugeWUIM))
 
   module.hot.accept('./reducers', () => {
     // eslint-disable-next-line global-require
@@ -33,8 +33,5 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
     store.replaceReducer(nextRootReducer)
   })
 
-  /* eslint-enable */
   if (window) window.deluge = deluge
-} else {
-  render(<DelugeWUIM deluge={deluge} store={store} />, appRoot)
 }
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,98 +1,95 @@
 import Webpack from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import NotifierPlugin from 'webpack-notifier'
 import path from 'path'
 
 const {
   PORT = 8080,
   HOST = '0.0.0.0',
   NODE_ENV = 'development',
-  BUILD_DIR = `${__dirname}/dist`,
+  BUILD_DIR = path.resolve(__dirname, 'dist'),
   WEBPACK_DEVTOOL = 'source-map',
 } = process.env
 
+const APP_DIR = path.resolve(__dirname, 'app')
+
 const config = {
-  context: `${__dirname}/app`,
   entry: [
     'react-hot-loader/patch',
-    './main.jsx',
+    `webpack-dev-server/client?http://localhost:${PORT}`,
+    'webpack/hot/only-dev-server',
+    './app/main.jsx',
   ],
   output: {
     path: BUILD_DIR,
-    filename: '[name].bundle.js',
-    chunkFilename: '[id].bundle.js',
+    filename: 'main.bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      include: [APP_DIR],
+      loader: 'babel-loader',
+    }, {
+      test: /\.css$/,
+      include: [APP_DIR],
+      loader: ExtractTextPlugin.extract({
+        loader: 'css-loader?sourceMap&modules&camelCase!postcss-loader',
+      }),
+    }, {
+      test: /\.css$/,
+      include: [/node_modules/],
+      loader: ExtractTextPlugin.extract({
+        loader: 'css-loader?sourceMap!postcss-loader',
+      }),
+    }, {
+      test: /\.html$/,
+      include: [APP_DIR],
+      loader: 'html-loader',
+    }, {
+      test: /\.json$/,
+      include: [APP_DIR],
+      loader: 'json-loader',
+    }, {
+      test: /\.(jpe?g|png|gif|webp|svg)$/i,
+      loaders: [
+        'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack-loader?bypassOnDebug?optimizationLevel=7&interlaced=false',
+      ],
+    }],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
-  devtool: WEBPACK_DEVTOOL,
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        exclude: /node_modules/,
-        loader: 'babel',
-      }, {
-        test: /\.css$/,
-        include: /app/,
-        exclude: /node_modules/,
-        loaders: [
-          'style?sourceMap',
-          'css?camelCase&modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]',
-          'postcss',
-          //'stylefmt?config=.stylelintrc',
-        ],
-      }, {
-        test: /\.css$/,
-        exclude: /app/,
-        include: /node_modules/,
-        loader: ExtractTextPlugin.extract(
-          'style?sourceMap',
-          'css',
-        ),
-      }, {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
-        ],
-      }, {
-        test: /\.json$/, loader: 'json',
-      },
+    modules: [
+      'node_modules',
+      APP_DIR,
     ],
-  },
-  devServer: {
-    hot: true,
-    inline: true,
-    historyApiFallback: true,
-    port: PORT,
-    host: HOST,
-    compress: true,
+    extensions: ['.js', '.json', '.jsx'],
   },
   plugins: [
     new Webpack.HotModuleReplacementPlugin(),
     new Webpack.NamedModulesPlugin(),
-    new Webpack.NoErrorsPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
+    new Webpack.NoEmitOnErrorsPlugin(),
     new Webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
     }),
-    new ExtractTextPlugin('app.css'),
-  ],
-}
-
-if (NODE_ENV === 'development') {
-  config.plugins.push(
-    new NotifierPlugin({
-      title: 'Deluge WUIM',
-      contentImage: path.join(__dirname, 'app/assets/deluge.png'),
-      alwaysNotify: false,
+    new ExtractTextPlugin({ filename: 'bundle.css', disable: false, allChunks: true }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(APP_DIR, 'index.html'),
     }),
-  )
+
+  ],
+  devServer: {
+    hot: true,
+    contentBase: BUILD_DIR,
+    publicPath: '/',
+    port: PORT,
+    historyApiFallback: true,
+    host: HOST,
+    noInfo: true,
+  },
+  devtool: WEBPACK_DEVTOOL,
+  context: __dirname,
+  target: 'web',
 }
 
 export default config
